ad4a15dc5e6d4b5ba76369e1cb9a9f418c93f7c5
judynliu <jun.liu38@qmail.cuny.edu>
2024-12-10 01:27:38 +0000

    completed form

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -7,6 +7,31 @@ export default function Page() {
         <input type="text" name="product" id="product" className="m-5 text-black 
         bg-yellow-400" />
         <br/>
+        <label htmlFor="cars" className="mx-5">Choose a car:</label>
+
+<select name="cars" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+<br />
+<div className="ml-5 mt-5">
+  <input type="radio" id="html" name="fav_language" value="HTML" />
+  <label htmlFor="html">HTML</label><br />
+  <input type="radio" id="css" name="fav_language" value="CSS" />
+  <label htmlFor="css">CSS</label><br />
+  <input type="radio" id="javascript" name="fav_language" value="JavaScript" />
+  <label htmlFor="javascript">JavaScript</label>
+  </div>
+
+  <div className="ml-5 mt-5">
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+  <label htmlFor="vehicle1"> I have a bike</label><br />
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+  <label htmlFor="vehicle2"> I have a car</label><br />
+  </div>
+<br />
         <button className="ml-10 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
       </form>
       </>
