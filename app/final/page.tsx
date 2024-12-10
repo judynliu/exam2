b45f48857545926eda509a07b058edca101ac723
judynliu <jun.liu38@qmail.cuny.edu>
2024-12-10 00:50:17 +0000

    button color

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         <label htmlFor="product">Product Description</label>
         <input type="text" name="product" id="product" className="text-black 
         bg-yellow-400" />
-        <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit">Save</button>
+        <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" type="submit">Save</button>
       </form>
       </>
     );
