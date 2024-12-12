1a003164cab710d35213504d749e2f25fff0992c
judynliu <jun.liu38@qmail.cuny.edu>
2024-12-12 20:14:25 +0000

    update

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -4,26 +4,26 @@ export default function Page() {
       <h1 className="my-5 text-2xl font-extrabold text-center text-pink-400">Our Skincare Products</h1>
       <form>
         <label htmlFor="product" className="ml-5 text-xl text-pink-200">Browse Products:</label>
-        <input type="text" name="product" id="product" className="m-5 text-black 
+        <input type="text" placeholder="Type here" name="product" id="product" className="m-5 text-black 
         bg-pink-200" />
         <br/>
         
 <label htmlFor="product type" className="mx-5 text-xl text-pink-200">Select Product Type:</label>
-<select name="type" id="type">
+<select class="select select-primary" name="type" id="type">
   <option value="cream">Cream</option>
   <option value="serum">Serum</option>
   <option value="cleanser">Cleanser</option>
   <option value="eye cream">Eye Cream</option>
 </select>
 <label htmlFor="skin type" className="mx-5 text-xl text-pink-200">Select Skin Type:</label>
-<select name="type" id="type">
+<select class="select select-primary" name="type" id="type">
   <option value="sensitive skin">Sensitive Skin</option>
   <option value="oily skin">Oily Skin</option>
   <option value="dry skin">Dry Skin</option>
   <option value="combination skin">Combination Skin</option>
 </select>
 <label htmlFor="skin concern" className="mx-5 text-xl text-pink-200">Select Skin Concern:</label>
-<select name="concern" id="concern">
+<select class="select select-primary" name="concern" id="concern">
   <option value="redness">Redness</option>
   <option value="lack of hydration">Lack of Hydration</option>
   <option value="signs of aging">Signs of Aging</option>
@@ -33,17 +33,17 @@ export default function Page() {
 
 <div className="ml-5 mt-5 text-xl text-pink-200">Retail Size:
 <br/>
-  <input type="radio" id="1 oz" name="size" value="1 oz" />
+  <input type="radio" id="1 oz" class="radio radio-primary" name="size" value="1 oz" />
   <label htmlFor="1 oz"> 1 oz</label><br />
-  <input type="radio" id="4 oz" name="size" value="4 oz" />
+  <input type="radio" id="4 oz" class="radio radio-primary" name="size" value="4 oz" />
   <label htmlFor="4 oz"> 4 oz</label><br />
-  <input type="radio" id="8 oz" name="size" value="8 oz" />
+  <input type="radio" id="8 oz" class="radio radio-primary" name="size" value="8 oz" />
   <label htmlFor="8 oz"> 8 oz</label>
   </div>
   <br />
 
   <label htmlFor="skin concern" className="mx-5 text-xl text-pink-200">Choose Product Rating:</label>
-<select name="rating" id="rating">
+<select class="select select-primary" name="rating" id="rating">
   <option value="below">Below 3 Stars</option>
   <option value="over">Over 3 Stars</option>
   <option value="full">5 stars</option>
@@ -51,17 +51,17 @@ export default function Page() {
 
 <h3 className="ml-5 mt-5 text-xl text-pink-200">Key Ingredients:</h3>
   <div className="ml-5 text-xl text-pink-200">
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Niacinamide" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Niacinamide" />
   <label htmlFor="niacinamide"> Niacinamide</label><br />
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Panthenol" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Panthenol" />
   <label htmlFor="panthenol"> Panthenol</label><br />
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Hyaluronic Acid" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Hyaluronic Acid" />
   <label htmlFor="hyaluronic"> Hyaluronic Acid</label><br />
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Retinol" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Retinol" />
   <label htmlFor="retinol"> Retinol</label><br />
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Vitamin C" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Vitamin C" />
   <label htmlFor="vitamin"> Vitamin C</label><br />
-  <input type="checkbox" id="Ingredients" name="Ingredients" value="Ceramides" />
+  <input type="checkbox" id="Ingredients" class="checkbox checkbox-primary" name="Ingredients" value="Ceramides" />
   <label htmlFor="ceramides"> Ceramides</label><br />
   </div>
 <br />
